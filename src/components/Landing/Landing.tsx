539e9f0969a270269b111106681e49ead8076eee
Mahmood <Mahmood>
2024-08-30 21:48:22 +0300

    Add Untested auth

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,10 +1,27 @@
+import { cache, createAsync, redirect } from "@solidjs/router";
 import About from "./About";
 import Features from "./Features";
 import Footer from "./Footer";
 import Header from "./Header";
 import Intro from "./InroPage";
+import { getLoggedInUser } from "~/lib/server/appwrite";
+
+
+const user = cache(async () => {
+  "use server"
+  const isLoggedIn = await getLoggedInUser()
+
+  if (isLoggedIn) {
+    throw redirect("/app")
+  }
+
+}, "app")
+
+
+
 
 export default function Landing() {
+  createAsync(async () => user())
   return (
     <>
       <Header />
